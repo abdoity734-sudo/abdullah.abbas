f1339b658b54eb60a9417471ae26d10111b7cd54
Abdullah Abbas <abdoity734@gmail.com>
2026-07-15 15:49:27 +0300

    java.s

--- myproject/task.js
+++ myproject/task.js
@@ -10,32 +10,26 @@ but.addEventListener("click", function () {
         return;
     }
 
-    // إنشاء عنصر المهمة
     const li = document.createElement("li");
 
-    // إنشاء Checkbox
     const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
+    checkbox.type = "checkbox"
 
-    // إنشاء النص
     const span = document.createElement("span");
     span.textContent = task;
 
-    // عند الضغط على Checkbox يتم شطب المهمة
     checkbox.addEventListener("change", function () {
         span.classList.toggle("done");
     });
 
-    // إنشاء زر الحذف
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "X";
 deleteBtn.id = "deleteBtn";
-    // حذف المهمة
+   
     deleteBtn.addEventListener("click", function () {
         li.remove();
     });
 
-    // إضافة العناصر داخل li
     li.appendChild(checkbox);
     li.appendChild(span);
     li.appendChild(deleteBtn);
